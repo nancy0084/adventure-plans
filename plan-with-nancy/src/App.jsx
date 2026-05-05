19f444aaa392443587f92a45a5efecaac60b4635
Nancy Arya <nancyy0804@gmail.com>
2026-05-05 14:31:47 +0530

    first commit

---
+++ plan-with-nancy/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import data from "./data";
+import Tours from "./components/Tours";
+import { useState } from "react";
+
+const App = () => {
+  const [tours, setTours] = useState(data);
+
+  function removeTour(id){
+    const newTours = tours.filter( tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(tours.length === 0){
+    return(
+      <div className="refresh">
+        <h2>Sorry😔<br/>No Tours Left</h2>
+        <button onClick={() => setTours(data)} className="btn-white">Refresh</button>
+      </div>
+    )
+  }
+  return (
+    <div className="App">
+      <Tours tours={tours} removeTour={removeTour}> </Tours>
+    </div>
+  );
+};
+
+export default App;
